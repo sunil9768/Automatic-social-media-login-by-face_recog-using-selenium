83198f58526f62497c53dd4f7b42be8c12c7798d
sunil kumar <41811960+sunil9768@users.noreply.github.com>
2019-02-22 06:36:05 -0500

    Add files via upload

---
+++ mainSunil.py
@@ -0,0 +1,15 @@
+print("choose your choice")
+print(" 1.Facebook\n 2.Instagram \n 3.Github \n 4.Linkedin")
+n=input()
+if n==str(1):
+	import fb
+elif n==str(2):
+   	import insta
+elif n==str(3):
+	import github
+elif n==str(4):
+	import linkedin
+else:
+	print("byy")
+    
+
