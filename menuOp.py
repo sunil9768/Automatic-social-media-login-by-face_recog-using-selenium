1e214c3f380b8cce7e40f003aa671dface891676
sunil kumar <41811960+sunil9768@users.noreply.github.com>
2019-02-22 06:36:56 -0500

    Add files via upload

---
+++ menuOp.py
@@ -0,0 +1,15 @@
+print("choose your choice")
+print(" 1.Facebook\n 2.Instagram \n 3.Github \n 4.Linkedin")
+n=input()
+if n==str(1):
+	import fbOp
+elif n==str(2):
+   	import instaOp
+elif n==str(3):
+	import githubOp
+elif n==str(4):
+	import linkedinOp
+else:
+	print("byy")
+    
+
