5df51ad2255c19327e779de056cabfb69b57e074
sunil kumar <41811960+sunil9768@users.noreply.github.com>
2019-02-25 01:02:06 -0500

    Add files via upload

---
+++ fbOp.py
@@ -0,0 +1,37 @@
+import selenium
+from selenium import webdriver
+from getpass import getpass
+from time import sleep
+
+
+use = "8559931989"
+pwd = ""
+#message=input('Enter your message for status:')
+
+
+
+browser = webdriver.Chrome('/home/sunil/Downloads/chromedriver_linux64(1)/chromedriver')
+browser.get('https://www.facebook.com/login')
+
+
+username_box = browser.find_element_by_id('email')
+username_box.send_keys(use)
+sleep(3)
+
+
+pwd_box=browser.find_element_by_id('pass')
+pwd_box.send_keys(pwd)
+sleep(3)
+
+
+fb_login=browser.find_element_by_id('loginbutton')
+fb_login.submit()
+sleep(3)
+
+#text_box =browser.find_element_by_xpath("//textarea[@name='xhpc_message']")
+#text_box.send_keys(message)
+#sleep(3)
+
+#post_bt=browser.find_element_by_xpath("//button[contains(.,'Share')]")
+#post_bt.click()
+#print("post done")
